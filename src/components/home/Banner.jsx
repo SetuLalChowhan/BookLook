a6bb1049d753c72bc8919385739b9420fa759f66
setu <setulalchowhan@gmail.com>
2025-04-21 23:04:56 +0600

    bannerImage added

--- src/components/home/Banner.jsx
+++ src/components/home/Banner.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import BannerImage from "@/assets/images/Banner.png"
 
 const Banner = () => {
   return (
     <section>
-        <div className='container'>
-            
+        <div className={`bg-cover bg-center bg-no-repeat w-full h-[743px]`} style={{backgroundImage:`url(${BannerImage})`}}>
 
         </div>
     </section>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div >
+    <div  className=" flex flex-col gap-12">
         <Banner/>
       
     </div>
